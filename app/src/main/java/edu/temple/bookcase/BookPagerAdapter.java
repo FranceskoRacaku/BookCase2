ccd7d6a907adb9b035ac2c3061a18be969d573df
Francesko <Franceskoracaku@hotmail.com>
2020-04-06 22:22:42 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/bookcase/BookPagerAdapter.java
@@ -0,0 +1,32 @@
+package edu.temple.bookcase;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+
+public class BookPagerAdapter extends FragmentStatePagerAdapter {
+    ArrayList<Book> bookCollection;
+
+    public BookPagerAdapter(@NonNull FragmentManager fm, ArrayList<Book> bookCollection) {
+        super(fm);
+        this.bookCollection = bookCollection;
+
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        BookTitleFragment fragment = BookTitleFragment.newInstance(bookCollection.get(position));
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return bookCollection.size();
+    }
+}
